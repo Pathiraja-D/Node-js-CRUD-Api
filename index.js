fb01f0b17fea4dbd570d6ec4a43e0f9802f2bf7b
Dinuk Pathiraja <139424859+Pathiraja-D@users.noreply.github.com>
2023-12-08 19:26:17 +0530

    cors added

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const port = 3000;
 const connectDb = require('./config/db');
 const userRoutes = require('./controller/user.controller');
+const cors = require('cors');
 
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/users',userRoutes);
 
